087bc30eed449921826e15a92f287eabe64ca50e
72 <72sevenzy2@gmail.com>
2025-10-03 22:35:41 +0500

    updated compiled ts, updated scripts, and ts config

--- dist/utils.d.ts
+++ dist/utils.d.ts
@@ -1,1 +1,4 @@
-export {};
+export declare function log(msg: string): void;
+export declare function error(msg: string): void;
+export declare function info(msg: string): void;
+export declare function sleep(ms?: number): Promise<void>;
--- dist/utils.js
+++ dist/utils.js
@@ -1,2 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.log = log;
+exports.error = error;
+exports.info = info;
+exports.sleep = sleep;
+function log(msg) {
+    return console.log(`[sniper] ${msg}`);
+}
+function error(msg) {
+    return console.error(`[sniper] ${msg}`);
+}
+function info(msg) {
+    return console.log(`[sniper] ${msg}`);
+}
+function sleep(ms = 1000) {
+    return new Promise((resolve) => { setTimeout(resolve, ms); });
+}
